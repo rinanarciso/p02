df8545916c3633f9fc7dec45012586df71a5e5cb
Rina Narciso <rnarciso@sfu.ca>
2016-10-27 15:05:28 -0700

    updated Reviews section (form and toggle)

---
+++ js/review-toggle.js
@@ -0,0 +1,10 @@
+//	Team R^2
+//	Rina Narciso
+//	Rosanne Wong
+//	IAT 339 D101 - P02 Company
+
+$(document).ready(function(){
+    $("#review-btn").click(function(){
+        $(".review-form").slideToggle();
+    });
+});
